092d47462311cbcca7e239e1500619ead12ef3de
lovetingyuan <1932294867@qq.com>
2020-06-08 16:34:49 +0800

    add view and copy

--- scripts/popup.js
+++ scripts/popup.js
@@ -3,18 +3,36 @@
   const translateBtn = document.getElementById('translate')
   const result = document.getElementById('result')
   const bgpage = chrome.extension.getBackgroundPage();
-
+  function copyTextToClipboard (text) {
+    const copyFrom = document.createElement("textarea");
+    copyFrom.textContent = text;
+    document.body.appendChild(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy');
+    copyFrom.blur();
+    document.body.removeChild(copyFrom);
+  }
   const translate = (text) => {
     if (!text) return
     translateBtn.value = '翻译中...'
     translateBtn.disabled = true
+    document.querySelectorAll('button').forEach(btn => {
+      btn.disabled = true
+    })
     bgpage.fetchTransApi(text).then(res => {
       translateBtn.value = '翻译'
       translateBtn.disabled = false
+      document.querySelectorAll('button').forEach(btn => {
+        btn.disabled = false
+      })
       if (res) {
+        result.innerHTML = ''
         result.textContent = res.translation.join(', ')
       } else {
-        result.textContent = '翻译失败'
+        result.innerHTML = '<span class="error-text">翻译失败</span>'
+        document.querySelectorAll('button').forEach(btn => {
+          btn.disabled = true
+        })
       }
     })
   }
@@ -22,9 +40,19 @@
     if (evt.keyCode === 13) {
       translate(origin.value)
     }
-    // return true
   })
   translateBtn.addEventListener('click', evt => {
     translate(origin.value)
+  })
+  document.querySelectorAll('button').forEach(btn => {
+    btn.addEventListener('click', evt => {
+      if (evt.target.id === 'copy') {
+        if (result.textContent) {
+          copyTextToClipboard(result.textContent)
+        }
+      } else if (evt.target.id === 'view') {
+        window.open('https://youdao.com/w/' + encodeURIComponent(origin.value))
+      }
+    })
   })
 })()
